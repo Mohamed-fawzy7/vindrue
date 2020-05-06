81645d152d3ab0f2d015446b2930b9ed3b58a1da
Mohamed Fawzy <mhmdsalm7777@gmail.com>
2020-05-06 23:59:22 +0200

    added cors

--- index.js
+++ index.js
@@ -14,6 +14,13 @@ mongoose.connect(dbLink, { useUnifiedTopology: true, useNewUrlParser: true }).th
     console.log("failed to connect to db");
 })
 
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-with, Content-type, Accept, Authorization");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS");
+    next();
+})
+
 
 app.use("", (req, res, next) => {
     console.log(req.path);
